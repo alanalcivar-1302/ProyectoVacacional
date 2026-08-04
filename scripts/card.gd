eef31f05a60f020664ba4492326d524986c88fb3
Alan Nahin Alcivar Gomez <alan.alcivar@epn.edu.ec>
2026-08-04 16:35:36 -0500

    Arreglo de errores

--- scripts/card.gd
+++ scripts/card.gd
@@ -3,26 +3,28 @@ extends Area2D
 @export var valor : int = 0
 @export var frente : Texture2D
 
+signal carta_seleccionada(carta)
+
 var boca_abajo := true
 
 @onready var sprite = $Sprite2D
 
+#Inicia el juego
 func _ready():
-	sprite.texture = load("res://assets/cards/dorso.png")
+	pass
 
+#revela las cartas y muestra la imagen de la carta
 func revelar():
 	boca_abajo = false
 	sprite.texture = frente
 
+#oculta la carta mostrando la imagen del dorso
 func ocultar():
 	boca_abajo = true
 	sprite.texture = load("res://assets/cards/dorso.png")
 
+#detecta los clicks
 func _input_event(viewport, event, shape_idx):
-	
 	if event is InputEventMouseButton:
-		
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			
-			revelar()
-			
+			emit_signal("carta_seleccionada", self)
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 @export var separacion_cartas := 170
 const CARD = preload("res://scenes/card.tscn")
 
+#Lista con todas las imagenes de las cartas
 var imagenes = [
 	preload("res://assets/cards/carta1.png"),
 	preload("res://assets/cards/carta2.png"),
@@ -9,33 +10,42 @@ var imagenes = [
 	preload("res://assets/cards/carta4.png")
 ]
 
+#Listas en donde estaran las cartas de jugador y cpu
 var mano_jugador = []
 var mano_cpu = []
 
+#El mazo por defecto para ambos
 var mazo = [1,2,3,4]
 
+#llama a las funciones que crean las cartas
 func _ready():
-	print("El juego inicio")
+	
 	crear_cartas()
 	
 	crear_cartas_cpu()
 
 func crear_cartas():
+	#con un bucle crea las 4 cartas
 	for i in range(4):
-		
-		var carta = CARD.instantiate()
-		
+		#Instancia las cartas 
+		var carta = CARD.instantiate() 
+		#Le da el valor a la carta
 		carta.valor = i + 1
-		
+		#asigna la imagen respectiva de cada carta
 		carta.frente = imagenes[i]
-		
+		#la hace hijo al nodo2D Cartas
 		$Jugador/Cartas.add_child(carta)
+		#Revela la carta
+		carta.revelar()
+		#configuracion de escala
 		carta.scale = Vector2(0.4,0.4)
+		#La coloca en el punto inicial.
 		var inicio = $Jugador/PuntoInicial.position
-		
+		#posiciona la carta
 		carta.position = inicio + Vector2(i * separacion_cartas ,0)
-		
+		#la almacena en la lista de las cartas
 		mano_jugador.append(carta)
+		carta.carta_seleccionada.connect(jugar_carta)
 		
 
 func crear_cartas_cpu():
@@ -46,14 +56,15 @@ func crear_cartas_cpu():
 		carta.valor = i + 1
 		
 		carta.frente = imagenes[i]
-		
 		$CPU/Cartas.add_child(carta)
+		carta.ocultar()
 		carta.scale = Vector2(0.4,0.4)
 		var inicio = $CPU/PuntoInicial.position
 		carta.position = inicio + Vector2(i * separacion_cartas,0)
 
 		mano_cpu.append(carta)
-		
+
+
 func mover_carta_jugador(carta):
 
 	carta.reparent($Mesa)
@@ -65,3 +76,9 @@ func mover_carta_cpu(carta):
 	carta.reparent($Mesa)
 
 	carta.global_position = $Mesa/PosCartaCPU.global_position
+
+func jugar_carta(carta):
+
+	carta.reparent($Mesa)
+
+	carta.global_position = $Mesa/PosCartaJugador.global_position
